9ce840fdb0fc7c00128032ad7aded3f8a7d3d1eb
Melissa Astbury <melissajastbury@gmail.com>
2020-09-15 14:25:23 +0100

    created a todolist component that rendered each todo individually. This is now being rendered through map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from "react";
-import { TodoListItem } from "./TodoListItem";
-import { Todo } from "./types";
+import React, { useState } from "react";
+import { TodoList } from "./TodoList";
+import { Todo, ToggleTodo } from "./types";
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "Paint nails", complete: false },
   { text: "Water the plants", complete: true },
   { text: "Cook a Sunday Roast", complete: false },
 ];
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <TodoListItem todo={todos[1]} />
+      <React.Fragment>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+      </React.Fragment>
     </div>
   );
 };
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Todo } from "./types";
+import { Todo, ToggleTodo } from "./types";
 
 import "./TodoListItem.css";
 
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
   return (
     <li>
       <label className={todo.complete ? "complete" : undefined}>
-        <input type="checkbox" checked={todo.complete} />
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />
         {todo.text}
       </label>
     </li>
